4c18ed3d3a4a3cca4dccca1d72db91e733c3dcad
Noureddin Alfaouri <nfaouri5@gmail.com>
2023-04-08 00:48:46 +0300

    build

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/antrollingshon/University/GRADUATION_PROJECT/a2b/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/Objects-normal/x86_64/path_provider_foundation-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/antrollingshon/University/GRADUATION_PROJECT/a2b/macos/Pods/Target\ Support\ Files/path_provider_foundation/path_provider_foundation-dummy.m \
+  /Users/antrollingshon/University/GRADUATION_PROJECT/a2b/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/module.modulemap \
+  /Users/antrollingshon/University/GRADUATION_PROJECT/a2b/macos/Pods/Target\ Support\ Files/path_provider_foundation/path_provider_foundation-prefix.pch
