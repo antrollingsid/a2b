174d78fe3e003eec0f80a7c156081bf58ac978fb
Noureddin Alfaouri <nfaouri5@gmail.com>
2023-04-08 08:25:08 +0300

    Revert "build"
    This reverts commit 4c18ed3d3a4a3cca4dccca1d72db91e733c3dcad.

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/antrollingshon/University/GRADUATION_PROJECT/a2b/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/antrollingshon/University/GRADUATION_PROJECT/a2b/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/Objects-normal/x86_64/path_provider_macos-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/antrollingshon/University/GRADUATION_PROJECT/a2b/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-dummy.m \
-  /Users/antrollingshon/University/GRADUATION_PROJECT/a2b/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/module.modulemap \
-  /Users/antrollingshon/University/GRADUATION_PROJECT/a2b/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-prefix.pch
