ae232141266e85b59fe70944350a444d1ff4b129
antrollingsid <nfaouri5@gmail.com>
2023-04-26 16:52:41 +0300

    Update colors.dart

--- lib/Components/assets/colors.dart
+++ lib/Components/assets/colors.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color backgroundLightMode = Colors.white;
-  static Color primaryDark = const Color.fromRGBO(187, 134, 252, 1.0);
-  static Color backgroundDark = const Color.fromRGBO(16, 16, 16, 1.0);
-  static Color secondary = const Color.fromRGBO(3, 218, 198, 1.0);
-  static Color buttonStroke = const Color.fromRGBO(33, 33, 33, 1.0);
-  static Color buttonYellow = const Color.fromRGBO(255, 246, 117, 1.0);
-  static Color buttonDark = const Color.fromRGBO(23, 23, 23, 1.0);
-  static Color secondaryBlue = const Color.fromRGBO(96, 191, 244, 1.0);
-  static Color buttonRed = const Color.fromRGBO(244, 48, 12, 1.0);
-  static Color buttonBlue = const Color.fromRGBO(17, 63, 118, 1.0);
-  static Color onBackgroundDark = Colors.white;
-  static Color textBlue = const Color.fromRGBO(17, 63, 118, 1.0);
-  static Color textLightBlue = const Color.fromRGBO(229, 242, 253, 1.0);
-  static Color textBlack = Colors.black;
+  static const Color backgroundLightMode = Colors.white;
+  static const Color onBackgroundDark = Colors.white;
+  static const Color backgroundDark = Color.fromRGBO(16, 16, 16, 1.0);
+  static const Color textLightBlue = Color.fromRGBO(229, 242, 253, 1.0);
+  static const Color secondaryBlue = Color.fromRGBO(96, 191, 244, 1.0);
+  static const Color buttonStroke = Color.fromRGBO(33, 33, 33, 1.0);
+  static const Color buttonYellow = Color.fromRGBO(255, 246, 117, 1.0);
+  static const Color primaryDark = Color.fromRGBO(187, 134, 252, 1.0);
+  static const Color buttonDark = Color.fromRGBO(23, 23, 23, 1.0);
+  static const Color buttonBlue = Color.fromRGBO(17, 63, 118, 1.0);
+  static const Color textBlack = Colors.black;
+  static const Color secondary = Color.fromRGBO(3, 218, 198, 1.0);
+  static const Color buttonRed = Color.fromRGBO(244, 48, 12, 1.0);
+  static const Color textBlue = Color.fromRGBO(17, 63, 118, 1.0);
 }
