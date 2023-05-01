68dd7e4d9b2ce22f34ed0758f04c8f9ee01d1c1c
antrollingsid <nfaouri5@gmail.com>
2023-05-01 22:29:23 +0300

    styling calendar

--- lib/User/screens/calender.dart
+++ lib/User/screens/calender.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import, import_of_legacy_library_into_null_safe, duplicate_import, prefer_final_fields, use_key_in_widget_constructors
 
 import 'package:a2b/User/screens/dashboard.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter/services.dart' show rootBundle;
@@ -50,54 +51,91 @@ class _PlaceOrderState extends State<PlaceOrderCalender> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TableCalendar(
-                calendarController: _controller, // use the controller here
-                initialCalendarFormat: CalendarFormat.month,
-                calendarStyle: CalendarStyle(
-                  todayColor: Colors.blue,
-                  selectedColor: Theme.of(context).primaryColor,
-                  todayStyle: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22.0,
-                    color: Colors.white,
-                  ),
+              Container(
+                width: 333,
+                height: 374,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 40, 40, 40),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                headerStyle: HeaderStyle(
-                  centerHeaderTitle: true,
-                  formatButtonDecoration: BoxDecoration(
-                    color: Colors.brown,
-                    borderRadius: BorderRadius.circular(22.0),
-                  ),
-                  formatButtonTextStyle: const TextStyle(color: Colors.white),
-                  formatButtonShowsNext: false,
-                ),
-                startingDayOfWeek: StartingDayOfWeek.monday,
-                // onDaySelected: (date, events) {
-                //   print(date.toUtc());
-                // },
-                builders: CalendarBuilders(
-                  selectedDayBuilder: (context, date, events) => Container(
-                    margin: const EdgeInsets.all(5.0),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(8.0),
+                child: TableCalendar(
+                  calendarController: _controller, // use the controller here
+                  initialCalendarFormat: CalendarFormat.month,
+                  daysOfWeekStyle: const DaysOfWeekStyle(
+                    weekendStyle: TextStyle(
+                      color: AppColors.textGrey,
                     ),
-                    child: Text(
-                      date.day.toString(),
-                      style: const TextStyle(color: Colors.white),
+                    weekdayStyle: TextStyle(
+                      color: AppColors.textGrey,
                     ),
                   ),
-                  todayDayBuilder: (context, date, events) => Container(
-                    margin: const EdgeInsets.all(5.0),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(8.0),
+                  calendarStyle: CalendarStyle(
+                    weekdayStyle: const TextStyle(
+                      color: AppColors.textGrey,
                     ),
-                    child: Text(
-                      date.day.toString(),
-                      style: const TextStyle(color: Colors.white),
+                    weekendStyle: const TextStyle(
+                      color: AppColors.textGrey,
+                    ),
+                    outsideWeekendStyle: const TextStyle(
+                      color: Color.fromARGB(255, 80, 80, 80),
+                    ),
+                    outsideStyle: const TextStyle(
+                      color: Color.fromARGB(255, 80, 80, 80),
+                    ),
+                    todayColor: AppColors.secondaryBlue,
+                    selectedColor: Theme.of(context).primaryColor,
+                    todayStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  headerStyle: const HeaderStyle(
+                    rightChevronIcon: Icon(
+                      Icons.chevron_right,
+                      color: AppColors.backgroundLightMode,
+                    ),
+                    leftChevronIcon: Icon(
+                      Icons.chevron_left,
+                      color: AppColors.backgroundLightMode,
+                    ),
+                    titleTextStyle: TextStyle(
+                      color: AppColors.backgroundLightMode,
+                    ),
+                    centerHeaderTitle: true,
+                    formatButtonVisible: false,
+                    formatButtonTextStyle: TextStyle(color: Colors.white),
+                    formatButtonShowsNext: false,
+                  ),
+                  startingDayOfWeek: StartingDayOfWeek.monday,
+
+                  // onDaySelected: (date, events) {
+                  //   print(date.toUtc());
+                  // },
+                  builders: CalendarBuilders(
+                    selectedDayBuilder: (context, date, events) => Container(
+                      margin: const EdgeInsets.all(5.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Text(
+                        date.day.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    todayDayBuilder: (context, date, events) => Container(
+                      margin: const EdgeInsets.all(5.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: AppColors.primaryDark,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Text(
+                        date.day.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
