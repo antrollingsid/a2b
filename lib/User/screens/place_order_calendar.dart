3b6aaadc3834aeb3f3d61dd01f6d4783c912c57f
smilekuakua <36351749+smilekuakua@users.noreply.github.com>
2023-05-10 12:45:47 +0300

    zx

--- lib/User/screens/place_order_calendar.dart
+++ lib/User/screens/place_order_calendar.dart
@@ -2,10 +2,12 @@
 
 import 'package:a2b/Components/widgets/custom_calendar.dart';
 import 'package:a2b/User/screens/place_order_upload.dart';
+import 'package:a2b/User/screens/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../main/utils/allConstants.dart';
 import '../../Components/widgets/app_bar_buttons.dart';
+import 'dashboard.dart';
 
 class PlaceOrderCalendar extends StatefulWidget {
   const PlaceOrderCalendar({Key? key});
@@ -25,72 +27,100 @@ class _PlaceOrderState extends State<PlaceOrderCalendar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: AppColors.backgroundDark,
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(80),
-        child: CustomAppBar(
-          titleText: 'Choose The Date',
+        resizeToAvoidBottomInset: false,
+        backgroundColor: AppColors.backgroundDark,
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(80),
+          child: CustomAppBar(
+            titleText: 'Choose The Date',
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              CustomCalendar(),
-              // CustomShip(),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CustomCalendar(),
+                // CustomShip(),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            backgroundColor: AppColors.buttonStroke,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'hhh',
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.access_time,
+                  color: AppColors.backgroundLightMode,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  height: 62,
+                  width: 62,
+                  decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                          colors: [
+                            AppColors.secondaryBlue,
+                            AppColors.primaryDark,
+                          ],
+                          begin: FractionalOffset(0.0, 1.0),
+                          end: FractionalOffset(1.0, 0.4),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                      borderRadius: BorderRadius.circular(50)),
+                  child: const Icon(
+                    Icons.add,
+                    color: AppColors.backgroundDark,
+                    size: 40,
+                  ),
+                ),
+                label: '',
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.feedback_outlined,
+                  color: AppColors.backgroundLightMode,
+                ),
+                label: '',
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_outlined,
+                  color: AppColors.backgroundLightMode,
+                ),
+                label: '',
+              ),
             ],
+            // currentIndex: _selectedIndex,
+            selectedItemColor: AppColors.primaryDark,
+            onTap: (index) {
+              if (index == 0) {
+                Get.to(const DashBoard());
+              } else if (index == 1) {
+                Get.to(const DashBoard());
+              } else if (index == 2) {
+                Get.to(const PlaceOrderUpload());
+              } else if (index == 3) {
+              } else if (index == 4) {
+                Get.to(const Profile());
+              }
+            },
           ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: AppColors.buttonStroke,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.access_time,
-              color: AppColors.backgroundLightMode,
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.add,
-              color: AppColors.backgroundLightMode,
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.feedback_outlined,
-              color: AppColors.backgroundLightMode,
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person_outlined,
-              color: AppColors.backgroundLightMode,
-            ),
-            label: '',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: AppColors.primaryDark,
-        onTap: (index) {
-          if (index == 2) {
-            Get.to(() => const PlaceOrderUpload());
-          }
-        },
-      ),
-    );
+        ));
   }
 }
