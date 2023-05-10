ccccee9578d2129c09a0e11f9f7a4d2167b2d484
antrollingsid <nfaouri5@gmail.com>
2023-05-10 15:16:19 +0300

    Update menu.dart

--- lib/User/screens/menu.dart
+++ lib/User/screens/menu.dart
@@ -28,6 +28,7 @@ class _EmptyPage extends State<EmptyPage> {
         preferredSize: Size.fromHeight(80),
         child: CustomAppBar(
           titleText: 'Menu',
+          isActionVisible: false,
         ),
       ),
       body: SingleChildScrollView(
