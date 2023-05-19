cd2683c9643f7eeba76922494c4bf18515ea8a89
antrollingsid <nfaouri5@gmail.com>
2023-05-19 03:33:23 +0300

    try/catch when app is initialized

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,5 +1,6 @@
 import 'package:a2b/controllers/auth_controller.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:nb_utils/nb_utils.dart';
@@ -7,12 +8,36 @@ import 'package:nb_utils/nb_utils.dart';
 import '../controllers/update_profile_controller.dart';
 import '../main/utils/data_providers.dart';
 
-Future<void> init() async {
-  await SharedPreferences.getInstance();
-  await initialize(aLocaleLanguageList: languageList());
-  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
-  Get.lazyPut(() => UpdateProfileController());
-  // Get.put(UpdateProfileController());
+// Future<void> init() async {
+//   await SharedPreferences.getInstance();
+//   await initialize(aLocaleLanguageList: languageList());
+//   await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+//   Get.lazyPut(() => UpdateProfileController());
+//   // Get.put(UpdateProfileController());
 
-  FlutterNativeSplash.remove();
+//   FlutterNativeSplash.remove();
+// }
+Future<void> init() async {
+  try {
+    await SharedPreferences.getInstance();
+    await initialize(aLocaleLanguageList: languageList());
+    await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+    Get.lazyPut(() => UpdateProfileController());
+    // Get.put(UpdateProfileController());
+
+    FlutterNativeSplash.remove();
+  } catch (e) {
+    if (e is PlatformException) {
+      if (e.code == 'null-error') {
+        print('Error: Platform returned null value for non-null return value');
+        // Handle the specific null value error
+      } else {
+        print('Platform Exception occurred: ${e.message}');
+        // Handle other types of PlatformExceptions
+      }
+    } else {
+      print('Error occurred during initialization: $e');
+      // Handle other types of errors
+    }
+  }
 }
